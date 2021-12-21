9b2dce896f7c886e733fa32bf3774207f793d359
lujun <luzhewei923>
2021-12-21 15:42:03 +0800

    all

--- src/nowcoder/questions/one/NC45_实现二叉树先序_中序和后序遍历.java
+++ src/nowcoder/questions/one/NC45_实现二叉树先序_中序和后序遍历.java
@@ -3,17 +3,14 @@ package nowcoder.questions.one;
 
 import nowcoder.entity.TreeNode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * NC45_实现二叉树先序_中序和后序遍历
- *
+ * <p>
  * 描述
  * 给定一棵二叉树，分别按照二叉树先序，中序和后序打印所有的节点。
- *
+ * <p>
  * 数据范围：0 \le n \le 10000≤n≤1000，树上每个节点的val值满足 0 \le val \le 1000≤val≤100
  * 要求：空间复杂度 O(n)O(n)，时间复杂度 O(n)O(n)
  *
@@ -26,11 +23,10 @@ public class NC45_实现二叉树先序_中序和后序遍历 {
     private List<Integer> postResult = new ArrayList<>();
 
     /**
-     *
      * @param root TreeNode类 the root of binary tree
      * @return int整型二维数组
      */
-    public int[][] threeOrders (TreeNode root) {
+    public int[][] threeOrders(TreeNode root) {
         // write code here
         travers(root);
         final int[][] res = new int[3][preResult.size()];
@@ -42,19 +38,41 @@ public class NC45_实现二叉树先序_中序和后序遍历 {
         return res;
     }
 
-    public void travers (TreeNode node){
-        if(node == null){
+    public void travers(TreeNode node) {
+        if (node == null) {
             return;
         }
-        //pre
+        //前序遍历
         preResult.add(node.val);
         travers(node.left);
-        //mid
+        //中序遍历
         midResult.add(node.val);
         travers(node.right);
-        //post
+        //后续遍历
         postResult.add(node.val);
 
+    }
+
+    //层级遍历
+    public void hierarchyTravers(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            final TreeNode cur = queue.poll();
+
+            System.out.println(cur.val);
+
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
     }
 
 }
